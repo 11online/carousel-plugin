50e276f6633a77d2d74a2d0878955bf972349745
Eric Debelak <ericdebelak@gmail.com>
2016-01-29 16:55:20 -0700

    quote slider

--- index.php
+++ index.php
@@ -1,102 +1,76 @@
 <?php
 /*
-Plugin Name: Carousel Plugin
+Plugin Name: Quote Plugin
 Plugin URI: http://www.11online.us
 Description: Easy way to edit Carousel JS
 Version: 1.0
-Revision Date: December 16, 2015
+Revision Date: January 28, 2016
 License: GNU General Public License 3.0 (GPL) http://www.gnu.org/licenses/gpl.html
 Author: Eric Debelak, Josh Garcia
 Author URI: http://www.11online.us
 */
 
-define('CAROUSEL_VERSION','1.0');
-define('CAROUSEL_NAME','Carousel Plugin');
-define('CAROUSEL_ROOT',dirname(__FILE__).'/');
-define('CAROUSEL_DIR',basename(dirname(__FILE__)));
+define('QUOTE_VERSION','1.0');
+define('QUOTE_NAME','Quote Plugin');
+define('QUOTE_ROOT',dirname(__FILE__).'/');
+define('QUOTE_DIR',basename(dirname(__FILE__)));
 
 // create custom plugin settings menu
-add_action('admin_menu', 'carousel_create_menu');
+add_action('admin_menu', 'quote_create_menu');
 
-function carousel_create_menu() {
+function quote_create_menu() {
 	//create new sub-level menu
-	add_submenu_page('options-general.php', 'Carousel Settings', 'Carousel Slider', 'administrator', 'Carousel', 'carousel_settings_page');
+	add_submenu_page('options-general.php', 'Quote Settings', 'Quote Slider', 'administrator', 'Quote', 'quote_settings_page');
 
 
 	//call register settings function
-	add_action( 'admin_init', 'register_carousel_settings' );
+	add_action( 'admin_init', 'register_quote_settings' );
 }
 
 // enqueue the admin js
-function carousel_admin_enqueue($hook) {
-	if($hook == 'settings_page_Carousel') {
+function quote_admin_enqueue($hook) {
+	if($hook == 'settings_page_Quote') {
 		wp_enqueue_media();
-		wp_enqueue_script( 'settings_page_script', plugin_dir_url( __FILE__ ) . 'views/js/settings.js' );
+		wp_enqueue_script( 'quote_settings_page_script', plugin_dir_url( __FILE__ ) . 'views/js/settings.js' );
 	}
 }
-add_action( 'admin_enqueue_scripts', 'carousel_admin_enqueue' );
+add_action( 'admin_enqueue_scripts', 'quote_admin_enqueue' );
 
 
-function register_carousel_settings() {
+function register_quote_settings() {
 	//register our settings
-	register_setting( 'carousel_settings_group', 'carousel_image_array' );
-	register_setting( 'carousel_settings_group', 'carousel_message_array' );
-	register_setting( 'carousel_settings_group', 'carousel_link_array' );
-	register_setting( 'carousel_settings_group', 'carousel_height' );
-	register_setting( 'carousel_settings_group', 'carousel_speed' );
+	register_setting( 'quote_settings_group', 'quote_message_array' );
 }
 
-function carousel_settings_page() {
+function quote_settings_page() {
 	// handle settings
   $messages = "";
-  if(isset($_POST['carousel_image_array'])) {
-      update_option("carousel_image_array", implode("|", $_POST['carousel_image_array']));
-      $messages = "Images updated.";
-  }
-  if(isset($_POST['carousel_message_array'])) {
-      update_option("carousel_message_array", implode("|", $_POST['carousel_message_array']));
-      $messages .= " Messages updated.";
-  }
-	if(isset($_POST['carousel_link_array'])) {
-      update_option("carousel_link_array", implode("|", $_POST['carousel_link_array']));
-      $messages .= " Links updated.";
-  }
-	if(isset($_POST['carousel_height'])) {
-      update_option("carousel_height", $_POST['carousel_height']);
-      $messages .= " Height updated.";
-  }
-	if(isset($_POST['carousel_speed'])) {
-      update_option("carousel_speed", $_POST['carousel_speed']);
-      $messages .= " Speed updated.";
+  if(isset($_POST['quote_message_array'])) {
+      update_option("quote_message_array", implode("|", $_POST['quote_message_array']));
+      $messages .= "Messages updated.";
   }
 
 	include_once('views/settings.php');
 }
 
-add_shortcode('carousel', 'carousel_shortcode_add_code');
+add_shortcode('quote', 'quote_shortcode_add_code');
 
-function carousel_shortcode_add_code() {
+function quote_shortcode_add_code() {
 
 	// add the js and css
-	add_action('wp_footer', 'carousel_add_code');
+	add_action('wp_footer', 'quote_add_code');
 
-	function carousel_add_code() {
-		wp_enqueue_style( 'bootstrap_carousel_style', plugin_dir_url( __FILE__ ) . '/bootstrap/css/bootstrap.css' );
-		wp_enqueue_script( 'bootstrap_carousel_script', plugin_dir_url( __FILE__ ) . '/bootstrap/js/bootstrap.min.js', array(), '1.0.0', true );
+	function quote_add_code() {
+		wp_enqueue_style( 'bootstrap_quote_style', plugin_dir_url( __FILE__ ) . '/bootstrap/css/bootstrap.css' );
+		wp_enqueue_script( 'bootstrap_quote_script', plugin_dir_url( __FILE__ ) . '/bootstrap/js/bootstrap.min.js', array(), '1.0.0', true );
 	}
 
-  $images = get_option("carousel_image_array");
-	$images = explode('|', $images);
-	$messages = get_option("carousel_message_array");
+	$messages = get_option("quote_message_array");
 	$messages = explode('|', $messages);
-	$links = get_option("carousel_link_array");
-	$links = explode('|', $links);
-	$height = get_option("carousel_height");
-	$speed = get_option("carousel_speed");
 	$text = '<div id="carousel" class="carousel slide" data-ride="carousel">
 	<!-- Indicators -->
 	<ol class="carousel-indicators">';
-	$count = count($images);
+	$count = count($messages);
 	for($i = 0; $i <  $count; $i++) {
 			if($i == 0) {
 				$text .= '<li data-target="#carousel" data-slide-to="0" class="active"></li>';
@@ -109,37 +83,19 @@ function carousel_shortcode_add_code() {
 	<div class="carousel-inner" role="listbox">';
   for($i = 0; $i <  $count; $i++) {
 				if($i == 0) {
-					$text .= '<a class="item active"';
+					$text .= '<div class="item active"';
 				} else {
-						$text .= '<a class="item"';
+						$text .= '<div class="item"';
 				}
-	$text .= ' style="background-image: url(' . stripslashes($images[$i]) . '); height: ' . $height . 'px; background-repeat: no-repeat; background-size: cover; background-position: center;" href="' . stripslashes($links[$i]) . '">
-			<div class="carousel-caption">
-				<h1>' . stripslashes($messages[$i]) . '</h1>
+	$text .= '>
+			<div>
+				<blockquote>' . stripslashes($messages[$i]) . '</blockquote>
 			</div>
-	</a>';
+	</div>';
 	}
 
-	$text .= '</div>
-	<!-- Controls -->
-	<a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
-	<span class="dashicons dashicons-arrow-left-alt2 glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-	<span class="sr-only">Previous</span>
-	</a>
-	<a class="right carousel-control" href="#carousel" role="button" data-slide="next">
-	<span class="dashicons dashicons-arrow-right-alt2 glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-	<span class="sr-only">Next</span>
-	</a>
-	</div>';
-	$text .= '<script>
-	jQuery(document).ready(function() {
-		jQuery(".carousel").carousel({
-			interval: ' . $speed . '
-		});
-	});
-	</script>';
+	$text .= '</div>';
 	return $text;
-
 }
 
 
--- views/settings.php
+++ views/settings.php
@@ -1,47 +1,24 @@
 <?php
-$carousel_image_array = get_option("carousel_image_array");
-$carousel_image_array = explode("|", $carousel_image_array);
-$carousel_message_array = get_option("carousel_message_array");
-$carousel_message_array = explode("|", $carousel_message_array);
-$carousel_link_array = get_option("carousel_link_array");
-$carousel_link_array = explode("|", $carousel_link_array);
-$carousel_height = get_option("carousel_height");
-$carousel_speed = get_option("carousel_speed");
+$quote_message_array = get_option("quote_message_array");
+$quote_message_array = explode("|", $quote_message_array);
 
 echo '<div class="wrap">
-	<div id="carousel-settings">
+	<div id="quote-settings">
 
-	    <h2>Carousel Settings</h2>
+	    <h2>Quote quote Settings</h2>
 
                 <form method="post" action="">
                     <table>
 										<tbody class="slideGroup"></tbody>';
-										$count = count($carousel_image_array);
+										$count = count($quote_message_array);
 										for($i = 0; $i <  $count; $i++) {
 											$slideCount = $i + 1;
 											echo '<tbody class="slideGroup"><tr><td><h3>Slide ' . $slideCount . '</h3></td></tr>';
-											echo '<tr><td><label for="carousel_image_array">Image Url</label></td>';
-					      			echo '<td><input class="slide' . $slideCount . '" name="carousel_image_array[]" type="text" value="' . stripslashes($carousel_image_array[$i]) . '"><input class="upload_image_button button" id="slide' . $slideCount . '" type="button" value="Upload Image" /></td></tr>';
-											echo '<tr><td><label for="carousel_message_array">Caption</label></td>';
-					      			echo '<td><input name="carousel_message_array[]" type="text" value="' . stripslashes($carousel_message_array[$i]) . '"></td></tr>';
-											echo '<tr><td><label for="carousel_link_array">Link</label></td>';
-					      			echo '<td><input name="carousel_link_array[]" type="text" value="' . stripslashes($carousel_link_array[$i]) . '"></td></tr>';
+											echo '<tr><td><label for="quote_message_array">Caption</label></td>';
+					      			echo '<td><input name="quote_message_array[]" type="text" value="' . stripslashes($quote_message_array[$i]) . '"></td></tr>';
 											echo '<tr class="deleteSlide"><td><a class="button button-primary">Delete This Slide</a></td></tr></tbody>';
 										}
-										echo '<tr><td>&nbsp;</td></tr><tr><td><a class="button button-primary" id="addSlide">Add a New Slide</a></td></tr>';
-										echo '<tr><td><h3>General Settings</h3></td></tr><tr>
-                        <td><label for="carousel_height">Height in pixels</label></td>
-                        <td><input type="number" name="carousel_height" id="carousel_height" value="';
-                        if($carousel_height) {
-                            echo stripslashes($carousel_height);
-                        }
-                        echo '"></td></tr><tr>
-												<td><label for="carousel_speed">Speed in Miliseconds</label></td>
-                        <td><input type="number" name="carousel_speed" id="carousel_speed" value="';
-                        if($carousel_speed) {
-                            echo stripslashes($carousel_speed);
-                        }
-                        echo '"></td>
+										echo '<tr><td>&nbsp;</td></tr><tr><td><a class="button button-primary" id="addSlide">Add a New Slide</a></td></tr>
                     </table>';
                     submit_button();
                 echo '</form>
